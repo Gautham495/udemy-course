586f1d2bfedb803de16481b01182443d348f63e1
Gautham495 <kinggautham495@gmail.com>
2022-01-16 11:41:41 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Git and GitHub from my Udemy Course and please share it
+         Git Course
         </a>
       </header>
     </div>
